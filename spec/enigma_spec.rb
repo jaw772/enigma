018d684ea59cbc84c42d1e8ead2144f7dab23e0e
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-11-12 11:28:47 -0700

    Test: initialize

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -2,6 +2,15 @@ require './lib/enigma'
 require './lib/encrypt'
 require './lib/decrypt'
 
-RSPec.describe Enigma do
-  let!(:enigma)(Enigma.new)
+RSpec.describe Enigma do
+  let!(:enigma){Enigma.new}
+
+
+  describe '#initialize' do
+    it "exists" do
+      expect(enigma).to be_a Enigma
+    end
+  end
+
+  describe
 end
