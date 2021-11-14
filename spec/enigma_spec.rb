42bb288751e2e8477d8467651e4f1ff389e957f4
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-11-13 21:45:32 -0700

    Stub: random_key

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -5,7 +5,6 @@ require './spec_helper'
 RSpec.describe Enigma do
   let!(:enigma){Enigma.new}
 
-
   describe '#initialize' do
     it "exists" do
       expect(enigma).to be_a Enigma
@@ -15,12 +14,29 @@ RSpec.describe Enigma do
   describe '#encrypt' do
     it "encrypts a given message" do
       expected = {
-        encryption: "keder ohulw",
+        encryption: "keder ohulw!",
         key: "02715",
         date: "040895"
       }
-      expect(enigma.encrypt("hello world", "02715", "040895")).to eq expected
+      expect(enigma.encrypt("hello world!", "02715", "040895")).to eq expected
     end
+
+    it "encrypts a given message for the current day" do
+      expected = {
+        encryption: "pkfawfqdzry",
+        key: "02715",
+        date: "131121"
+      }
+      expect(enigma.encrypt("hello world", "02715")).to eq expected
+    end
+
+    it "encrypts a given message for the current day with a random key" do
+      encrypted = enigma.encrypt("hello world")
+
+      allow(encrypted[:key]).to receive(:random_key)
+    end
+
+
   end
 
   describe '#decrypt' do
@@ -31,6 +47,15 @@ RSpec.describe Enigma do
         date: "040895"
       }
       expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq expected
+    end
+    it "decrypts a given message for the current day" do
+      expected = {
+        decryption: "hello world",
+        key: "02715",
+        date: "131121"
+      }
+      encrypted = enigma.encrypt("hello world", "02715")
+      expect(enigma.decrypt(encrypted[:encryption], "02715")).to eq expected
     end
   end
 end
