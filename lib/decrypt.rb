6295701f2f8bcdcbd21fe3c001bd5cd1e4226da7
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-11-13 18:55:44 -0700

    Feat: decrypts

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,2 +1,47 @@
+require_relative './modules/offsets'
+require_relative './modules/keys'
+
 class Decrypt
-end 
+  include Offsets
+  include Keys
+  def initialize(message, key, date)
+    @message = message
+    @key = key
+    @date = date
+    @key_hash = self.create_keys(key)
+    @offset_hash = self.create_offset(date)
+    @shifts = @key_hash.values
+    @letters = [*'a'..'z',' ']
+    @enigma_hash = {
+      key: @key,
+      date: @date
+    }
+  end
+
+  def shifts
+    shift = -1
+    @offset_hash.values.each do |offset|
+      shift += 1
+      @shifts[shift] += offset
+    end
+  end
+
+  def decrypts
+    shifts
+    shift = -1
+    coded_msg = ""
+    message_array = @message.chars
+    message_array.each do |char|
+      int = @letters.index(char)
+      if shift == 3
+        shift = -1
+      end
+      shift += 1
+      @mm = int - @shifts[shift]
+      e = @letters.rotate(@mm)
+      coded_msg.concat(e[0])
+    end
+    @enigma_hash[:decryption] = coded_msg
+    @enigma_hash
+    end
+  end
