628f8c3860d676667bb89cf079db22eb9ab85ef9
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-11-13 21:45:58 -0700

    Feat: random_key

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,18 +1,22 @@
 require './modules/keys'
-
+require 'date'
 class Enigma
   include Keys
+  @@dd = Date.today
   def initialize
   end
 
-  def encrypt(message, key, date)
+  def encrypt(message, key = random_key, date = @@dd.strftime("%d%m%y"))
     encrypt = Encrypt.new(message, key, date)
     encrypt.encrypts
   end
 
-  def decrypt(message, key, date)
+  def decrypt(message, key = random_key, date = @@dd.strftime("%d%m%y"))
     decrypt = Decrypt.new(message, key, date)
     decrypt.decrypts
   end
 
+  def random_key
+    rand.to_s[2..6]
+  end
 end
