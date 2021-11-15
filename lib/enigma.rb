d39889e74d4ab41d77aa798e32fe5630241e946b
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2021-11-15 14:48:29 -0700

    Move encrypts to encrypt on enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,11 +6,11 @@ class Enigma
   include Offsets
   include Keys
 
-  @@dd = Date.today
   def initialize
     @shift_counter = -1
     @letters = [*'a'..'z',' ']
     @shift_array = []
+    @dd = Date.today
 
   end
 
@@ -23,12 +23,38 @@ class Enigma
     end
   end
 
-  def encrypt(message, key = random_key, date = @@dd.strftime("%d%m%y"))
-    encrypt = Encrypt.new(message, key, date)
-    encrypt.encrypts
+  def encrypt(message, key = random_key, date = @dd.strftime("%d%m%y"))
+    @enigma_hash = {
+      key: key,
+      date: date
+    }
+    @shift_counter = -1
+    key_hash = self.create_keys(key)
+    offset_hash = self.create_offset(date)
+    shift_array = key_hash.values
+    message = message.downcase
+    shifts(offset_hash, shift_array)
+    coded_msg = ""
+    message_array = message.chars
+    message_array.each do |char|
+      if @letters.include?(char) == true
+        int = @letters.index(char)
+        if @shift_counter == 3
+          @shift_counter = -1
+        end
+        @shift_counter += 1
+        rotate_counter = (int + @shift_array[@shift_counter])
+        e = @letters.rotate(rotate_counter)
+        coded_msg.concat(e[0])
+      else
+        coded_msg.concat(char)
+      end
+    end
+    @enigma_hash[:encryption] = coded_msg
+    @enigma_hash
   end
 
-  def decrypt(message, key = random_key, date = @@dd.strftime("%d%m%y"))
+  def decrypt(message, key = random_key, date = @dd.strftime("%d%m%y"))
     @enigma_hash = {
       key: key,
       date: date
